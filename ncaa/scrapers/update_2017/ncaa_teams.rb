174f001d882489a56597c35d814844d1ebb2dd06
BarstoolRodeo <lewisgraeber@gmail.com>
2017-03-24 12:26:52 -0500

    Update ncaa_teams.rb

--- ncaa/scrapers/update_2017/ncaa_teams.rb
+++ ncaa/scrapers/update_2017/ncaa_teams.rb
@@ -1,18 +1,16 @@
 #!/usr/bin/env ruby
 
-#returning 3 rows of unwanted data that will impede output when sourced in other scrapers. will still work if deleted.  working on fix. 
-
 require 'csv'
 
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-year = 2015
+year = 2017
 division = 1
-#for year in 2012..2017
+
 CSV.open("csv/ncaa_teams_#{year}_D#{division}.csv","w",{:col_sep => "\t"}) do |ncaa_teams|
-user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
 
 # Header for team file
 
@@ -22,13 +20,12 @@ ncaa_teams << ["year", "year_id", "team_id", "team_name", "team_url"]
 #base_url = 'http://anonymouse.org/cgi-bin/anon-www.cgi/stats.ncaa.org'
 base_url = 'http://stats.ncaa.org'
 
-
+#for year in 2012..2014
 #	year_division_url = "http://anonymouse.org/cgi-bin/anon-www.cgi/http://stats.ncaa.org/team/inst_team_list?sport_code=MBA&academic_year=#{year}&division=#{division}&conf_id=-1&schedule_date="
 	year_division_url = "http://stats.ncaa.org/team/inst_team_list?sport_code=MBA&academic_year=#{year}&division=#{division}&conf_id=-1&schedule_date="
 #	print "\n#{year_division_url}"
 	
-	
-	valid_url_substring = "teams/"	#index/#{year_id}?org_id=" #still returning 3 rows of unwanted data. add syntax after /
+	valid_url_substring = "team/" ##{year_id}?org_id="
 
 	print "\nRetrieving division #{division} teams for #{year} ... "
 
@@ -51,7 +48,7 @@ base_url = 'http://stats.ncaa.org'
 
 		# NCAA team_id
 
-		team_id = link_url.split("/")[2] #changed
+		team_id = link_url.split("/")[2]
 
 		# NCAA team name
 
@@ -75,4 +72,3 @@ base_url = 'http://stats.ncaa.org'
 
 #ncaa_teams.close
 end
-#end
